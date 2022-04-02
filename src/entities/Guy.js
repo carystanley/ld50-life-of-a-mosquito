8149b4448026766d2ac27793024de865bfd60ddb
Cary Stanley <cary@yahoo-inc.com>
2022-04-02 10:02:50 -0700

    FlipX

--- src/entities/Guy.js
+++ src/entities/Guy.js
@@ -2,18 +2,36 @@ const HEAD_Y = 57;
 const BODY_Y = 77;
 
 class Guy {
-    constructor (scene, x) {
-        this.speed = 10;
+    constructor (scene, x, direction = 1) {
+        this.speed = 100;
+        this.direction = direction;
 
         this.headSprite = scene.physics.add.sprite(x, HEAD_Y, 'guyHead');
 
         this.bodySprite = scene.physics.add.sprite(x, BODY_Y, 'guyBody');
         this.bodySprite.play('guyBody-run');
-        this.bodySprite.setVelocityX(this.speed);
+        this.updateSprite();
     }
 
-    update (time, delta) {
+    update () {
+        /* Handle Bounds */
+        if (this.bodySprite.x > 340) {
+            this.direction = -1;
+            this.updateSprite();
+        } else if (this.bodySprite.x < 20) {
+            this.direction = 1;
+            this.updateSprite();
+        }
+
+        /* Keep Head on */
         this.headSprite.x = this.bodySprite.x;
+    }
+
+    updateSprite() {
+        this.bodySprite.setVelocityX(this.speed * this.direction);
+        const flip = this.direction !== 1;
+        this.bodySprite.setFlipX(flip);
+        this.headSprite.setFlipX(flip);
     }
 }
 
